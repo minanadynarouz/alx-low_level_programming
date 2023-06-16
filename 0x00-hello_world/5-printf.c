0b0cdeff962b1b5f3c6e9d0120d080d59e56d47f
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-16 10:08:45 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * out some text
+ * return should always be 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
