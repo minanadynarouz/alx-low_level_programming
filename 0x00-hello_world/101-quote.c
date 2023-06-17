56f637ea1793b61425316562e15533a78d841efd
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-16 18:30:31 -0700

    amend

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
  * Return: Always 1 (Success)
  */
 
-int main(void) 
+int main(void)
 {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    write(STDERR_FILENO, message, sizeof(message) - 1);
+write(STDERR_FILENO, message, sizeof(message) - 1);
     
-    return (1);
+return (1);
 }
