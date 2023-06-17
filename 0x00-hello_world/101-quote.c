9d59f20244d2a8ecacc1b4b99c2920ecbf60bdbf
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-16 18:25:16 -0700

    amend 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main(void)
-{
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+
+int main(void) {
+    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(STDERR_FILENO, message, sizeof(message) - 1);
+    return 1;
 }
+
