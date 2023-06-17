810229115480963a8fb63ebc5c2a9132e91b5d48
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-16 19:48:07 -0700

    submit 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
