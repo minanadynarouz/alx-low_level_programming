71ea8557c2e89623679ba876ee57f65628846623
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-16 18:26:58 -0700

    amend 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,11 @@
  * Return: Always 1 (Success)
  */
 
-int main(void) {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int main(void) 
+{
+    const char message[] = "and that piece of art is useful
+	    	\" - Dora Korpar, 2015-10-19\n";
     write(STDERR_FILENO, message, sizeof(message) - 1);
-    return 1;
+    return (1);
 }
 
