344183260b3941e9cb56c22f2b82390e10b67f29
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-16 19:30:04 -0700

    submit 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d",num);
+	}
+
+	printf("\n");
+
+	return (0);
+}
