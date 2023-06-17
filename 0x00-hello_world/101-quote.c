e2ff4c832571229a323f969e9272cf2a6a20d083
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-16 18:34:43 -0700

    amned

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 const char message[] = "and that piece of art is useful\" - "
-                      "Dora Korpar, 2015-10-19\n";
+	"Dora Korpar, 2015-10-19\n";
 
 write(STDERR_FILENO, message, sizeof(message) - 1);
     
