ae27ccc68ad4e16df271e9e95e0b5c78f02c45cb
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-19 11:32:31 -0700

    amend 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,29 +8,30 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    int i;
-    int j;
+	int i, j;
 
-    for (i = '0'; i < '9'; i++)
-    {
-        for (j = i + 1; j <= '9'; j++)
-        {
-            if (i != j)
-            {
-                putchar(i);
-                putchar(j);
+	for (i = '0'; i < '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+				
+				if (i == '8' && j == '9')
+				{
+					continue;
+				}
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 
-                if (i == '8' && j == '9')
-                    continue;
-
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-
-    return (0);
+	return (0);
 }
