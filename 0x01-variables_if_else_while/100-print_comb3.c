c372bcb205da2efde0c1a9b53c744c3b7f0a461b
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-19 11:26:13 -0700

    submitting 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+    int j;
+
+    for (i = '0'; i < '9'; i++)
+    {
+        for (j = i + 1; j <= '9'; j++)
+        {
+            if (i != j)
+            {
+                putchar(i);
+                putchar(j);
+
+                if (i == '8' && j == '9')
+                    continue;
+
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
+
+    return (0);
+}
