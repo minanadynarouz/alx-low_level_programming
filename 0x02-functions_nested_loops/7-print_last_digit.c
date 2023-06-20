79a8073e2b1731bfe224563d9557d386a95a5f5d
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-20 14:48:49 -0700

    amend 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit - function return last digit of a num
  * @n: Is number to make it abs.
- * Return: last digit of num 
+ * Return: last digit of num.
  */
 
 int print_last_digit(int n)
