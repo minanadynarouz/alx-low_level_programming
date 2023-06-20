b1b80471dd0a18a8c041387e88c5db61ba0797f7
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-20 14:41:32 -0700

    submit task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function return last digit of a num
+ * @n: Is number to make it abs.
+ * Return: last digit of num 
+ */
+
+int print_last_digit(int)
+{
+	return n % 10;
+}
