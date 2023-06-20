a688665a320dce72667c652041701a1a90eca6ae
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-20 15:54:25 -0700

    amend 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,25 +11,25 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		putchar('0');
+		_putchar('0');
 
 		for (j = 1; j <= 9; j++)
 		{
-			putchar(',');
-			putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 
 			mult = i * j;
 
 			if (mult <= 9)
 			{
-				putchar(' ');
+				_putchar(' ');
 			}
 			else
 			{
-				putchar((mult / 10) + '0');
+				_putchar((mult / 10) + '0');
 			}
-			putchar((mult % 10) + '0');
+			_putchar((mult % 10) + '0');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
