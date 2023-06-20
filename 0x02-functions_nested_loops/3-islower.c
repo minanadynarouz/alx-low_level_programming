ef986c861862bab4c2c7a683b17e56188d4af594
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-20 14:05:26 -0700

    submit task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - Check if char is lower char or not
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
