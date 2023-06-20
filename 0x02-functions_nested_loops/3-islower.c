5bb9025a9f0b292dbe51df1f8bab19f12229321b
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-20 14:06:57 -0700

    amend

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,8 @@
 
 /**
  * _islower - Check if char is lower char or not
+ * char c is parameter for the function that represent the character to be checked
+ * return 1 if true and return 0 if false
  */
 
 int _islower(int c)
