f4562fd152e2632262dd5de7adb440568c63b703
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-20 14:35:20 -0700

    amend 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,9 @@
 /**
  * _abs - function return the absolute value of num
  * @n: Is number to make it abs.
- * return 0 if n equal to zero, absolute value if n < 0, return number as it is otherwise
+ * Return: 0 if n equal to zero, 
+ * absolute value if n < 0, return 
+ * number as it is otherwise
  */
 
 int _abs(int n)
