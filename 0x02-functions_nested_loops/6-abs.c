56ed1df30fdd15b3f1b052e91d85f0055b767b8b
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-20 14:29:15 -0700

    submit task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - function return the absolute value of num
+ * @n: Is number to make it abs.
+ */
+
+int _abs(int n)
+{
+	if (n == 0 || n == -0)
+	{
+		return (0);
+	}
+	else if (n < 0)
+	{
+		return n * (-1);
+	}
+}
