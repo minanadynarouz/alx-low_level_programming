94981edfff23f61300c99c4f19ca5b225947c5ff
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-20 15:53:48 -0700

    amend 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,37 +8,28 @@
 void times_table(void)
 {
 	int i, j, mult;
+
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		putchar('0');
+
+		for (j = 1; j <= 9; j++)
 		{
+			putchar(',');
+			putchar(' ');
+
 			mult = i * j;
+
 			if (mult <= 9)
 			{
-				_putchar(mult + '0');
-				if (j == 9)
-				{
-					continue;
-				}
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				putchar(' ');
 			}
 			else
 			{
-				_putchar(mult / 10 + '0');
-				_putchar(mult % 10 + '0');
-				if (j == 9)
-				{
-					continue;
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				putchar((mult / 10) + '0');
 			}
+			putchar((mult % 10) + '0');
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
