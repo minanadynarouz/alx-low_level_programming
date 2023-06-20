dc0ca86d63958c7f11a8678d4633cbefe86b5c0f
minanadynarouz <mina.n.narouz@gmail.com>
2023-06-20 15:57:27 -0700

    submit 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - function add two numbers
+ * @x, @y: are parameter numbers to be added
+ * Return: return sum of two numbers
+ */
+
+int add(int x, int y)
+{
+	return x + y;
+}
