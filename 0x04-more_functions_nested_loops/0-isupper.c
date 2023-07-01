a79a9378d2135199cc0e380a483329a05cf77a61
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-01 05:00:39 -0700

    amend

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
-/* _isupper: check if character is uppercase or not
-* @c: is variable to bee checked
-* Return: 1 if upper, 0 if else
-*/
+/** 
+ * _isupper: check if character is uppercase or not
+ * @c: is variable to bee checked
+ * Return: 1 if upper, 0 if else 
+ */
 
 int _isupper(int c)
 {
