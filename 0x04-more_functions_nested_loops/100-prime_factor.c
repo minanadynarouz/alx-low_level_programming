7797c9cc5856e4f4d57b0ea8e001e75caada0c78
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-02 07:54:51 -0700

    submit 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - program to get the prime factor of number 612852475143.
+ * Return - 0 when the program ends.
+ */
+
+int main(void)
+{
+	long num = 612852475143;
+	int i;
+
+	for (i = (int)sqrt(num); i > 2; i++)
+	{
+		if (num % i == 0)
+		{
+			printf("%d\n", i);
+			break;
+		}
+	}
+
+	return (0);
+}
