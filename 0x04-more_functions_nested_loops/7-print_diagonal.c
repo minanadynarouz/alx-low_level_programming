6e79e9917c21578783680460e5283ba43a39a251
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-02 00:50:46 -0700

    submit task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,9 +17,14 @@ void print_diagonal(int n)
 
 		for (i = 1; i <= n; i++)
 		{
-			for (j = 0; j < i; j++)
+			for (j = 1; j <= i; j++)
 			{
-				_putchar('\\');
+				_putchar(' ');
+				
+				if (j == i)
+				{
+					_putchar('\\');
+				}
 			}
 
 		_putchar('\n');
