788f4ce32958d796d28420f0f9dda0b9050a6d41
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-02 03:14:48 -0700

    submit task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle using #.
+ * @size: The number of times to print triangle.
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j, l;
+
+		for (i = size - 1; i >= 0; i--)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar('.');
+			}
+			for (l = size; l > i; l--)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+
+		}
+	}
+}
