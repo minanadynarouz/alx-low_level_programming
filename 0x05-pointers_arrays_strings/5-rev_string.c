cce63467e0801bd29baa5b39e0bfff19b7ef6d22
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-04 14:49:19 -0700

    submit task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverse a string.
+ * @s: string var to be reversed.
+ */
+
+void rev_string(char *s)
+{
+	int i, j, temp, counter = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		counter++;
+	}
+	
+	for (i = 0; j = counter - 1; i < j; i++; j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+
+	putchar('\n');
+}
