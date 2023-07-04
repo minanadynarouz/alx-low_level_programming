a5e65e2b80f84d87a901db0305f375eda4d144fc
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-04 14:27:36 -0700

    submit task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,5 +7,17 @@
 
 void print_rev(char *s)
 {
+	int i, j, counter = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		counter++;
+	}
+
+	for (j = counter - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
 }
