ebada28bc29944fc85171bac550cc02d7758f099
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-06 10:40:23 -0700

    submit task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: 
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1, num = 0; index = 0, i;
+
+	for (i = 0; i < s[i] != '\0'; i++)
+	{
+		if ((s[i] >= '0') && (s[i] <= '9'))
+		{
+			if (s[i - 1] == '-')
+			{
+				sign = -1;
+				index = 1;
+			}
+		}
+	}
+
+	while (s[index] != '\0')
+	{
+		if (s[index] >= '0' && s[index] <= '9')
+		{
+			num = num * 10 + s[index] - '0';
+		}
+
+		index++;
+	}
+
+	num = num * sign;
+
+	return (num);
+}
