be3f5e02712981755e3ff056226c89bc0430c54d
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-06 09:32:56 -0700

    submit task 7

---
+++ 0x05-pointers_arrays_strings/7*puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - function to put second half of give string
+ * @str: given string to be checked
+ */
+
+void puts_half(char *str)
+{
+	int i, half_len = 0, len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		half_len = len / 2;
+
+		for (i = half_len; i <= len; i++)
+		{
+			putchar(str[i]);
+		}
+	}
+	else
+	{
+		half_len = (len - 1) / 2;
+
+		for (i = half_len; i <= len; i++)
+		{
+			putchar(str[i]);
+		}
+	}
+
+	putchar('\n');
+}
