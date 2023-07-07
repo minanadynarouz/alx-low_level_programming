fd8ece2b6674909ae0f0ad6622a24fdff13b4d3f
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-07 00:58:40 -0700

    submit task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,24 +8,18 @@
 
 char *generatePassword(int length)
 {
-    // Define the characters allowed in the password
     const char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-    int numCharacters = sizeof(characters) - 1; // Calculate the number of characters
+    int numCharacters = sizeof(characters) - 1;
 
-    // Allocate memory for the password
     char *password = malloc((length + 1) * sizeof(char));
 
-    // Generate each character of the password
     for (int i = 0; i < length; i++)
     {
-        // Generate a random index within the range of characters
         int randomIndex = rand() % numCharacters;
 
-        // Assign the randomly selected character to the password
         password[i] = characters[randomIndex];
     }
 
-    // Add a null terminator at the end of the password
     password[length] = '\0';
 
     return password;
@@ -35,15 +29,12 @@ int main(void)
 {
     srand(time(NULL));
 
-    int passwordLength = 10; // Set the desired length of the password
+    int passwordLength = 10;
 
-    // Generate a password
     char *password = generatePassword(passwordLength);
-
-    // Print the generated password
+    
     printf("Generated Password: %s\n", password);
 
-    // Free the allocated memory for the password
     free(password);
 
     return 0;
