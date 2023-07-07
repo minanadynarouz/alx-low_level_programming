d9b0c5edb88c4898666da77d2397d74ca7977c91
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-07 02:09:53 -0700

    submit task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - function concatenate two strings together
+ * @dest: is string to recieve another one
+ * @src: is string to bee sent to another.
+ * Return: the new string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, counter_src = 0, counter_dest = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		counter_src++;
+	}
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		counter_dest++;
+	}
+
+	for (i = counter_dest; i < counter_dest + counter_src; i++)
+	{
+		*(dest + i) = *(src + i - counter_dest); // copy the rest
+	}
+
+	return (dest);
+}
