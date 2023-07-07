32600253330282dd35717f098533e0564fb9bdf3
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-07 04:37:40 -0700

    subimit 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize all words in string.
+ * @c: string to be capitalized.
+ * Return: capitalized char*
+ */
+
+char *cap_string(char *c)
+{
+	int i, j;
+	char delimeters[] = " \t\n,;.!?\"(){}";
+
+	for (i = 1; c[i] != '\0'; i++)
+	{
+		for (j = 0; delimeters[j] != '\0'; j++)
+			if (c[i - 1] == delimeters[j])
+			{
+				if (c[i] >= 'a' && c[i] <= 'z')
+				{
+					c[i] -= 32;
+				}
+			}
+	}
+	return (c);
+}
