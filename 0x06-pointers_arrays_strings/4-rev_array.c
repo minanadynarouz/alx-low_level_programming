3b764d8d47f38afa2c77f9dbeff66e0a6a959932
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-07 03:39:29 -0700

    submit task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse content of an array
+ * @a: array itself
+ * @n: array size
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, tmp;
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+}
