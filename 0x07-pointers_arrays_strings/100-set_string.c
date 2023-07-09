0907e0784e424a0418ef150f941bed9bed003c74
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-09 11:58:16 -0700

    amend

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/*
- * set_string - to copy from pointer to double pointer
- * @s: double pointer to assign new value for
- * @to: pointer to send data
+/**
+ * set_string - set value of a pointer to a char
+ * @s: Double pointer to recieve
+ * @to: Pointer to give
+ * Return: Void
  */
-
 void set_string(char **s, char *to)
 {
 	*s = to;
