d3b3021b4eb7ccbe11679fa0e61a11d8a761f848
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-09 09:14:17 -0700

    amend task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _memset - function to fill bytes 
+ * _memset - function to fill bytes
  * @s: pointer pointing to memory space
  * @b: var holding value will be used in memory space
  * @n: size of area to be filled
- * Return: A pointer 
+ * Return: A pointer
  */
 
 char *_memset(char *s, char b, unsigned int n)
