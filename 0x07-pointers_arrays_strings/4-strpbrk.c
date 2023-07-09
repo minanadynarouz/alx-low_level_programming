622f3a2a74a37353a85e8089c99a95b97d30b242
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-09 10:36:23 -0700

    submit task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates a character in a string
+ * @s: string to be checked
+ * @accept: characters to search for
+ * Return: pointer from the first occurrance of the char
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+}
