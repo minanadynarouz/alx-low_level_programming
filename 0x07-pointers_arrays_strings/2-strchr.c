fe040e2427369d00738a0831ba1e651687e5518e
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-09 09:50:01 -0700

    amend

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,6 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
@@ -18,6 +17,5 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 	}
-	
 	return (0);
 }
