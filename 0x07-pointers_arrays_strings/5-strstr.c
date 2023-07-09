521ce12e474578a848c6e2e1c748386b9ceb4058
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-09 10:56:52 -0700

    sumit 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,15 +11,17 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		for (j = 0; needle[j] != '\0' && haystack[i + j] == needle[j]; j++)
 		{
-			if (needle[i] == haystack[j])
-			{
-				return (haystack + j);
-			}
+		}
+
+		if (needle[j] == '\0')
+		{
+			return haystack + i;
 		}
 	}
-	return (0);
+
+	return NULL;
 }
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print a chessboard
+ * @a: 2d array to print the board
+ */
+
+void print_chessboard(char (*a)[8])
+{
+}
