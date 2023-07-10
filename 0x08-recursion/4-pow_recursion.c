bad188c698566ac9de5de344cda4fe5c43515b70
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-10 14:56:43 -0700

    submit task 4 - recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raise a number to power of another
+ * @x: number to raise it
+ * @y: power of the number
+ * Return: value of x to power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return x * _pow_recursion(x, y - 1);
+}
