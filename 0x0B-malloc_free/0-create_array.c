83ef28f9559d7c8321f00121d1f5c41d1fab9f7e
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-11 15:57:01 -0700

    submit task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size <= 0)
+	{
+		return (NULL);
+	}
+
+	char *arr = (char *)malloc(sizeof(char) * size);
+	
+	int i;
+
+	for(i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	return (arr);
+}
