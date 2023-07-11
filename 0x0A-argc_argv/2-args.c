1b927e010e0156a9c779860f5fccb75cdfe00e4b
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-11 12:56:33 -0700

    submit task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - is main program
+ * @argc: counter of arguments
+ * @argv: string arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n",argv[i]);
+	}
+
+	return (0);
+}
