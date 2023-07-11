f0617b55ce4e02ae019377daa242c81574438094
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-11 12:48:47 -0700

    submit task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - is main program
+ * @argc: counter of arguments
+ * @argv: string arguments
+ * Return: 0
+ */
+
+int main(int __attribute__((unused)) argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+tasks for argc and argv[]
