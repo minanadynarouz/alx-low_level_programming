b95df456c0860301a2b6224d2365139a5cb640b0
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-11 16:01:23 -0700

    amend

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,16 +5,21 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
+        unsigned int i;
+	
 	if (size <= 0)
 	{
 		return (NULL);
 	}
 
-	char *arr;
-
-	int i;
 	arr = (char *)malloc(sizeof(char) * size);
 
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	
 	for(i = 0; i < size; i++)
 	{
 		arr[i] = c;
