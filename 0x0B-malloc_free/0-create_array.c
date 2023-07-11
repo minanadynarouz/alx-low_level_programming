e339192478f7e4cfd89596b7a9660d7ee57f6277
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-11 16:04:01 -0700

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
+ * create_array - function to create array in heap
+ * @size: size var to be created
+ * @c: character to be inserted in new array
+ * Return: Pointer to first element of array
  */
 
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
-        unsigned int i;
-	
+	unsigned int i;
+
 	if (size <= 0)
 	{
 		return (NULL);
@@ -19,8 +23,8 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	
-	for(i = 0; i < size; i++)
+
+	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
