d364c8b81c2724aa7e5bba2c98a32599c2d4bc64
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-11 15:40:20 -0700

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,11 +14,12 @@ int main(int argc, char *argv[])
 	int coins[] = {25, 10, 5, 2, 1};
 	int cents;
 	int i;
-	int counter = 0;
+	int counter;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
+		
 		return (1);
 	}
 
@@ -27,11 +28,13 @@ int main(int argc, char *argv[])
 	if (cents < 0)
 	{
 		printf("%d\n", 0);
+		
 		return (0);
 	}
 
 	i = 0;
-	
+	counter = 0;
+
 	while (cents > 0)
 	{
 		if (cents >= coins[i])
@@ -46,5 +49,6 @@ int main(int argc, char *argv[])
 	}
 
 	printf("%d\n", counter);
+	
 	return 0;
 }
