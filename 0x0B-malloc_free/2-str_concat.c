3e173403717c901a0c6b1b1443334989b2d2f161
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-11 16:39:00 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,13 @@ char *str_concat(char *s1, char *s2)
 	int i, len;
 	int index_counter;
 
-	if (s1 == NULL || s1 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 
 	for (i = 0, len = 0; s1[i] || s2[i]; i++)
