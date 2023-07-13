e73e3ed03db1401b9a6db23253042e0cbd02f6f9
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-13 13:12:09 -0700

    amend

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * free_grid - function free double pointer grid
+ * @grid: the grid
+ * @height: height of the grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	
+	free(grid);
+	grid = NULL;
+}
