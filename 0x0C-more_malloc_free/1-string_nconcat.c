f863215eaf4256c8c2979ec08594598b09589638
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-14 05:58:28 -0700

    submit task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenate strings based on size n
+ * @s1: first string
+ * @s2: second string
+ * @n: size of bytes to be copies form s2 to s1.
+ * Return: char pointer with new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, j, k;
+	int len_s1 = 0;
+
+	char *newStr;
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+		n = 0;
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		len_s1++;
+	}
+	
+	newStr = malloc(sizeof(char) * (len_s1 + n + 1));
+	if (newStr != NULL)
+	{
+		for (i = 0, j = 0; i < len_s1; i++, j++)
+		{
+			newStr[i] = s1[i];
+		}
+
+		for (k = 0; k < n; j++, k++)
+		{
+			newStr[j] = s2[k];
+		}
+		newStr[j] = '\0';
+		return (newStr);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
