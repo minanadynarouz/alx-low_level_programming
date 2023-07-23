2dba9c9bd381a1fe4cbedb82c5a63493be0978a0
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-23 13:03:45 -0700

    amend

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,9 +19,8 @@ struct dog
 };
 
 /**
- *dog_t - alias name for struct dog.
- */
-
+*dog_t - Typedef for struct dog
+*/
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
