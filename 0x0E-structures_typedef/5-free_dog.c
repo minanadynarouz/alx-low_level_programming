7119b571da3f76b842a821cffa4175bed703e157
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-23 13:50:02 -0700

    submit 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * free_dog - function to free dog struct
+ * @d: is the struct to be freed
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
