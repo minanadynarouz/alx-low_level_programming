059f4ec29c7b932661a8c42183e5b9284099f29e
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-23 13:01:35 -0700

    amend

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,8 +12,7 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	
-	printf("Name: %s\n", d->name ? d->name: "(nil)");
+	printf("Name: %s\n", d->name ? d->name: "(nil)" );
 	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", d->owner ? d->owner: "(nil)");
-}	
+	printf("Owner: %s\n", d->owner ? d->owner: "(nil)" );
+}
