1b332a9c0c62904fc9cbf371bb3b18b0acfbf01e
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-23 13:47:06 -0700

    submit 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -26,5 +26,7 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+int length(char *str);
+char *copy(char *dest, char *src);
 
 #endif
