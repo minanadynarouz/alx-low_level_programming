116aa0af405b870999821d8663cff01f1ce6330c
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-24 09:27:32 -0700

    submit 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function to get index of found int.
+ * @array: array to iterate over.
+ * @size: size of array.
+ * @cmp: pointer to function we will use.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) > 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
