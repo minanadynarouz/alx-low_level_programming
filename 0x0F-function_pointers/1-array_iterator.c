56113a9f198fbab62b07d6f6ef8ccc94887c60c0
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-24 09:16:47 -0700

    amend 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (size == 0 || action == NULL)
+	if (array == NULL || action == NULL)
 	{
 		return;
 	}
@@ -19,5 +19,4 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	{
 		action(array[i]);
 	}
-	
 }
