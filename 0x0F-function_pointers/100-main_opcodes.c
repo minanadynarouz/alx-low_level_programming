2671f7b4b3d06a8d04fbd25592ddc90917ca16c4
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-25 08:56:26 -0700

    submit 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,10 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 /**
- */
+ * main - Entry point that will print hex values.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: 0
+*/
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
+	int i, num;
+	unsigned char *ptr = (unsigned char*)main;
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit (1);
+	}
+	
+	num = atoi(argv[1]);
+	
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < num; i++)
+	{
+		printf("%02x", ptr[i]);
+		if (i < num - 1)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+	return (0);
 }
