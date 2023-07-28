69d10fc5302371484bae1264579f4cf4f4a762b0
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-28 01:04:09 -0700

    submit 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,4 +7,37 @@
  * Return: address of the new element, or NULL if it failed
  */
 
+list_t *add_node_end(list_t **head, const char *str)
+{
+	size_t l;
+	list_t *newNode;
 
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode->str = strdup(str);
+
+	for (l = 0; str[l] != '\0'; l++)
+		;
+
+	newNode->len = l;
+	newNode->next = NULL;
+
+	if ((*head) == NULL)
+	{
+		(*head) = newNode;
+	}
+	else
+	{
+		list_t *temp = (*head);
+		while ((temp)->next != NULL)
+		{
+			temp = (temp)->next;
+		}
+		(temp)->next = newNode;
+	}
+	return (*head);
+}
