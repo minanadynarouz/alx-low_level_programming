f3771764658578c863751e80cd8e9fa7a936fd03
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-28 00:41:31 -0700

    submit 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,5 +9,22 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	
+	list_t *new;
+	size_t l;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	for (l = 0; str[l] != '\0'; l++)
+	{
+	}
+	new->len = l;
+	new->next = (*head);
+	*head = new;
+
+	return (*head);
 }
