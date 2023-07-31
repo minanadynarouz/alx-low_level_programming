5cd1ceb338a2b4a2eb9ebba16fbe686e244b5dc8
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-31 14:14:57 -0700

    submit 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -24,6 +24,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if ((*head) == NULL)
 	{
 		(*head) = new;
+		return (*head);
 	}
 	else
 	{
@@ -35,5 +36,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		}
 		(temp)->next = new;
 	}
-	return (new);
+	return (*head);
 }
