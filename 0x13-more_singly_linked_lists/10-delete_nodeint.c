ca86dd73fee75a818c89bdde5307f511250300fa
minanadynarouz <mina.n.narouz@gmail.com>
2023-08-01 20:52:41 -0700

    submit 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete specific node at
+ * mentioned index num.
+ * @head: double pointer to SLL.
+ * @index: index to delete from.
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *run = *head;
+	listint_t *deleteNode;
+	unsigned int i;
+
+	if (index != 0)
+	{
+		for (i = 0; i < index - 1 && run != NULL; i++)
+		{
+			run = run->next;
+		}
+	}
+	
+	if (run == NULL || (run->next == NULL && index != 0))
+		return (-1);
+
+	deleteNode = run->next;
+
+	if (index != 0)
+	{
+		run->next = deleteNode->next;
+		free(deleteNode);
+	}
+	else
+	{
+		free(run);
+		*head = deleteNode;
+	}
+	return (1);
+}
