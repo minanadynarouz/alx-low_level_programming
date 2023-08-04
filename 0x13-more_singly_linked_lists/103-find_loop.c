16685090aa33c8afb7edef025ec7a8c51b5afa95
minanadynarouz <mina.n.narouz@gmail.com>
2023-08-04 02:38:00 -0700

    amend

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - function to find loop in list.
+ * @head: pointing to start of list.
+ * Return: address of the node where the loop starts.
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	int diff;
+	listint_t *temp;
+
+	if (!head)
+		return (0);
+
+	while (head)
+	{
+		diff = head - head->next;
+		if (diff > 0)
+		{
+			head = head->next;
+		}
+		else
+		{
+			temp = head->next;
+			break;
+		}
+	}
+	return (temp);
+}
