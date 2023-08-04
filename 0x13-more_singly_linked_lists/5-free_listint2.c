2143f1c2087b92816c466af707489b9f4997eca2
minanadynarouz <mina.n.narouz@gmail.com>
2023-08-04 01:12:18 -0700

    amend

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,14 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *cursor;
-	listint_t **temp = head;
+	listint_t *temp;
 
-	if (temp != NULL)
+	if (head == NULL)
+		return;
+	while (*head != NULL)
 	{
-		while (*head != NULL)
-		{
-			cursor = *head;
-			free(cursor);
-			*head = (*head)->next;
-		}
-		*temp = NULL;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 }
