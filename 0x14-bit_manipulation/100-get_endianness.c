87061558451fb05d33039259f3cd95c2819c35bf
minanadynarouz <mina.n.narouz@gmail.com>
2023-08-05 03:00:50 -0700

    amend

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,8 +2,15 @@
 
 /**
  * get_endianness - determine little or big endian.
- * union: is union used to test.
  * Return: 0 if big endian, 1 if little endian.
+ */
+
+/**
+ * struct description - A union to store 
+ * an integer and its individual bytes.
+ * @value: The integer value stored in the union.
+ * @bytes: An array of bytes representing the 
+ * integer value.
  */
 
 int get_endianness(void)
