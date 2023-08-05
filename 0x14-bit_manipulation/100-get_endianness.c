d3761346222b7a824d3e46ff5f4ef501af0cd03a
minanadynarouz <mina.n.narouz@gmail.com>
2023-08-05 02:57:21 -0700

    submit endianess task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_endianness - determine little or big endian.
+ * Return: 0 if big endian, 1 if little endian.
+ */
+
+int get_endianness(void)
+{
+	union
+	{
+		unsigned int value;
+		unsigned char bytes[sizeof(unsigned int)];
+	} data;
+
+	data.value = 1;
+
+	if (data.bytes[0] == 1)
+	{
+		return (1);
+	}
+	return (00);
+}
