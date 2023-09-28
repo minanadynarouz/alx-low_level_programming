560f7102b87501d965c30204f1ce12551239bc9b
Mina Nady Narouz <131834381+minanadynarouz@users.noreply.github.com>
2023-09-28 23:28:25 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,15 +16,15 @@ int wildcmp_rec(char *s1, char *s2, int c1, int c2)
 		return (1);
 	}
 
-	if (s2[c2] == '*')
-	{
-		return (wildcmp_rec(s1, s2, c1, c2 + 1) || wildcmp_rec(s1, s2, c1 + 1, c2));
-	}
 	if (s1[c1] == s2[c2] || s2[c2] == '?')
 	{
 		return (wildcmp_rec(s1, s2, c1 + 1, c2 + 1));
 	}
 
+	if (s2[c2] == '*')
+	{
+		return (wildcmp_rec(s1, s2, c1, c2 + 1) || wildcmp_rec(s1, s2, c1 + 1, c2));
+	}
 	return (0);
 }
 
