c9edfa7e9cff0cafff3abbff05c05201490cc00b
minanadynarouz <mina.n.narouz@gmail.com>
2023-09-28 21:58:20 -0700

    amending code

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,33 +9,14 @@
  * Return: 1 in case match, 0 otherwise
  */
 
-int wildcmp_rec(char *s1, char *s2, int c1, int c2)
+int wildcmp(char *s1, char *s2)
 {
-	if (s1[c1] == '\0' && s2[c2] == '\0')
-	{
-		return (1);
-	}
-
-	if (s1[c1] == s2[c2] || s2[c2] == '?')
-	{
-		return (wildcmp_rec(s1, s2, c1 + 1, c2 + 1));
-	}
-
-	if (s2[c2] == '*')
-	{
-		return (wildcmp_rec(s1, s2, c1, c2 + 1) || wildcmp_rec(s1, s2, c1 + 1, c2));
-	}
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == *s1)
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 	return (0);
 }
 
-/**
- * wildcmp - functions to run the above function
- * @s1: first string
- * @s2: second string
- * Return: 1 in case match, 0 otherwise
- */
-
-int wildcmp(char *s1, char *s2)
-{
-	return (wildcmp_rec(s1, s2, 0, 0));
-}
