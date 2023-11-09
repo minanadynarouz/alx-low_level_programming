2b733f947fe7da2342723e3a08e4f859dad88b3f
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-09 12:42:32 -0800

    submit 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - func to provide num of elements in DLL.
+ * @h: DLL
+ * Return: count of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h);
+{
+	const dlistint_t *head = h;
+	size_t len = 0;
+
+	while (head != NULL)
+	{
+		printf("%d\n", head->n);
+		len++;
+		head = head->next;
+	}
+	return (len);
+}
