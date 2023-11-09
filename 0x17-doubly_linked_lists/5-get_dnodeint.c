60552eeacd54ce54a9c775005e82d919e58377e0
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-09 14:57:22 -0800

    amend

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,12 +14,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	i = 0;
 	while (head != NULL)
 	{
-		i++;
-		head = head->next;
 		if (i == index)
 		{
 			return (head);
 		}
+		i++;
+		head = head->next;
 	}
 	return (NULL);
 }
