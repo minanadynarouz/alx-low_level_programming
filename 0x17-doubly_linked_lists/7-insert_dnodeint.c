23f19a3d9ae39f3be21be3abbcabcb248a6899ab
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-10 02:24:09 -0800

    amend

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,44 +8,41 @@
  * Return: the address of the new node, or NULL if it failed.
  */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n) {
-    if (!h) return NULL; // Handle invalid input
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current = *h;
+	unsigned int i = 0;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	
+	if (!new_node) return (NULL);
+	if (!h) return (NULL);
 
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    if (!new_node) return NULL; // Check for memory allocation failure
+	new_node->n = n;
+	new_node->next = new_node->prev = NULL;
+	if (!*h || idx == 0)
+	{
+		new_node->next = *h;
+		if (*h) (*h)->prev = new_node;
+		*h = new_node;
+		return (*h);
+	}
+	while (current && i < idx - 1)
+	{
+		current = current->next;
+		i++;
+	}
 
-    new_node->n = n;
-    new_node->next = new_node->prev = NULL;
+	if (!current)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = current->next;
+	current->next = new_node;
+	new_node->prev = current;
 
-    if (!*h || idx == 0) {
-        // Insert at the beginning if the list is empty or at the specified index
-        new_node->next = *h;
-        if (*h) (*h)->prev = new_node;
-        *h = new_node;
-        return *h;
-    }
-
-    dlistint_t *current = *h;
-    unsigned int i = 0;
-
-    while (current && i < idx - 1) {
-        current = current->next;
-        i++;
-    }
-
-    if (!current) {
-        free(new_node);
-        return NULL; // Index out of bounds
-    }
-
-    new_node->next = current->next;
-    current->next = new_node;
-    new_node->prev = current;
-
-    if (new_node->next) {
-        new_node->next->prev = new_node;
-    }
-
-    return *h;
+	if (new_node->next)
+		new_node->next->prev = new_node;
+	return (*h);
 }
 
