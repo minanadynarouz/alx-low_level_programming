a0ac5205b9a0d67c674d4f9b11e1140f8d84e8af
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-11 00:43:53 -0800

    amend

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,17 +1,49 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - func to provide num of elements in DLL.
+ * @h: DLL
+ * Return: count of elements
+ */
+
+unsigned int dllist_len(dlistint_t *h)
+{
+	dlistint_t *head = h;
+	size_t len = 0;
+
+	while (head != NULL)
+	{
+		len++;
+		head = head->next;
+	}
+	return (len);
+}
+
+/**
  * add_node - add new node
  * @node: node to create
  * @n: value of node's n.
  */
 
-dlistint_t * add_node(int n)
+dlistint_t * add_node(dlistint_t **h,unsigned int idx, int n)
 {
-	dlistint_t *node = malloc(sizeof(dlistint_t));
-	if (node == NULL)
-		return (NULL);
+	unsigned int i;
+	dlistint_t *current = *h;
+	dlistint_t *node;
+	
+	i = 0;
+	while (current != NULL)
+	{
+		current = current->next;
+		i++;
+	}
+        if (idx > i)
+                return (NULL);
 
+	node = malloc(sizeof(dlistint_t));
+        if (node == NULL)
+                return (NULL);
+	
 	node->n = n;
 	node->next = NULL;
 	node->prev = NULL;
@@ -34,7 +66,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
         dlistint_t *current = *h;
         dlistint_t *pre_current = *h;
 
-	new_node = add_node(n);
+	new_node = add_node(h, idx, n);
+	if (new_node == NULL)
+		return (NULL);
 
         if (*h == NULL)
         {
@@ -43,46 +77,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
         }
 
         if (idx == 0)
-        {
-                new_node->next = (*h)->next;
-                *h = new_node;
-                new_node->prev = *h;
-                return (new_node);
-        }
-        i = 0;
-    while (current != NULL)
-        {
-                current = current->next;
-                i++;
-        }
-        if (idx > i)
-        {
-                return (NULL);
-        }
+		return (add_dnodeint(h, n));
 
-        current = *h;
-        i = 0;
-        while (current != NULL)
-        {
-                if (i == idx - 1)
-                        break;
-                pre_current = current;
-                current = current->next;
-                i++;
-        }
-        if (current == NULL && i == idx - 1)
-        {
-                pre_current->next = new_node;
-                new_node->prev = pre_current;
-                new_node->next = NULL;
-                return (new_node);
-        }
-        current = *h;
+	i = dllist_len(current);
+	if (i == idx)
+		return (add_dnodeint_end(h, n));
 
         i = 0;
         while (current != NULL)
         {
-                if (i == idx)
+		if (i == idx)
                         break;
                 pre_current = current;
                 current = current->next;
@@ -94,6 +98,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
         if (current != NULL)
                 current->prev = new_node;
-
         return (new_node);
 }
