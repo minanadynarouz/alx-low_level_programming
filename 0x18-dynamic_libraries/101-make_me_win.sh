84ce118f206a833a70a25d3bdfcdd97d7edee60a
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-14 09:04:28 -0800

    amend

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp/ https://raw.github.com/minanadynarouz/alx-low_level_programming/0x18-dynamic_libraries/inject.so
-export LD_PRELOAD=/$PWD/inject.so 0x18.c/gm 9 8 10 24 75 9
+export LD_PRELOAD=/$PWD/inject.so
