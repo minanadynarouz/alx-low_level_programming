2f6a856a9c6c47e55f86e0c81f8270850946e756
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-02 03:11:10 -0800

    amend

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *new_hash_table = malloc(sizeof(hash_node_t));
-	
+
 	if (new_hash_table == NULL)
 		return (NULL);
 
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new_hash_table);
 		return (NULL);
 	}
-	
+
 	new_hash_table->size = size;
 
 	for (i = 0; i < size; i++)
