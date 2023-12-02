4fe51026eb0e3e5c0d1e6b61c90ba0d158705e4f
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-02 03:19:59 -0800

    Amending files

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *new_hash_table = malloc(sizeof(hash_node_t));
+	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
 
 	if (new_hash_table == NULL)
 		return (NULL);
