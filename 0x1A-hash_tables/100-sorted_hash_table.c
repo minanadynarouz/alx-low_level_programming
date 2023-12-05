15de7e5a89397aeeee8acedbcebeb590925a81cd
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-04 23:20:09 -0800

    amend

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,25 +1,5 @@
 #include "hash_tables.h"
 
-unsigned long int hash_djb2(const unsigned char *str)
-{
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *str++))
-
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
-}
-
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{
-	unsigned long int index = hash_djb2(key) % size;
-	return (index);
-}
-
 /**
  * shash_table_create - Creates a sorted hash table.
  * @size: The size of new sorted hash table.
